fca6292d6e5f37dd45686d840b437be03fa54b06
leehj8896 <leehj8896@kookmin.ac.kr>
2019-06-05 16:22:39 +0900

    2019년도 1학기 C++ 프로젝트 PushBox 게임 구현

---
+++ PushBox/main.cpp
@@ -0,0 +1,269 @@
+#include <ncurses.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+class Game
+{
+private:
+    int numOfStages;
+    vector<vector<vector<int>>> maps;
+    vector<pair<int, int>> playerPositions;
+    vector<vector<pair<int, int>>> goalPositions;
+    vector<bool> finished;
+    int currentStage;
+    int stepCount, pushCount;
+
+public:
+    //bool getFinished() { return finished; };
+    int getCurrentStage() { return currentStage; }
+    int getStepCount() { return stepCount; }
+    int getPushCount() { return pushCount; }
+    vector<bool> getFinised() { return finished; }
+    vector<vector<vector<int>>> getMaps() { return maps; }
+
+    void gameStart();
+    void setMaps();
+    void setPlayer();
+    void move(int direction);
+    void walk(int y, int x, int nextY, int nextX);
+    void push(int y, int x, int nextY, int nextX, int afterY, int afterX);
+    bool checkSuccess();
+    bool checkGoal(int y, int x);
+    void nextStage();
+    bool checkAllSuccess();
+};
+
+void Game::move(int direction)
+{
+    int y = playerPositions[currentStage].first;
+    int x = playerPositions[currentStage].second;
+    int nextY, nextX, afterY, afterX;
+    if (direction == KEY_UP)
+    {
+        nextY = y - 1;
+        nextX = x;
+        afterY = y - 2;
+        afterX = x;
+    }
+    else if (direction == KEY_DOWN)
+    {
+        nextY = y + 1;
+        nextX = x;
+        afterY = y + 2;
+        afterX = x;
+    }
+    else if (direction == KEY_LEFT)
+    {
+        nextY = y;
+        nextX = x - 1;
+        afterY = y;
+        afterX = x - 2;
+    }
+    else if (direction == KEY_RIGHT)
+    {
+        nextY = y;
+        nextX = x + 1;
+        afterY = y;
+        afterX = x + 2;
+    }
+
+    //다음칸이 벽이 아닐 때
+    if (maps[currentStage][nextY][nextX] != 1)
+    {
+        //다음칸이 비어있을 때 그냥 앞으로 전진
+        if (maps[currentStage][nextY][nextX] == 0 || maps[currentStage][nextY][nextX] == 3)
+            walk(y, x, nextY, nextX);
+        //다음칸이 상자일 때
+        else
+            //다음다음칸이 빈칸일 경우 PUSH
+            if (maps[currentStage][afterY][afterX] == 0 || maps[currentStage][afterY][afterX] == 3)
+            push(y, x, nextY, nextX, afterY, afterX);
+
+        //원래 있던 자리가 목적지인지 체크하여 변경
+        if (checkGoal(y, x))
+            maps[currentStage][y][x] = 3;
+        //목적지에 모두 상자가 차있는지 체크
+        if (checkSuccess())
+            nextStage();
+    }
+}
+
+//다음 스테이지로 가는 함수
+void Game::nextStage()
+{
+    finished[currentStage] = true;
+    currentStage++;
+    stepCount = 0;
+    pushCount = 0;
+}
+
+//모든 스테이지가 완료되었는지 체크하는 함수
+bool Game::checkAllSuccess()
+{
+    for (int i = 0; i < finished.size(); i++)
+        if (finished[i] == false)
+            return false;
+    return true;
+}
+
+//현재 스테이지가 완료되었는지 체크하는 함수
+bool Game::checkSuccess()
+{
+    int y, x;
+    for (int i = 0; i < goalPositions[currentStage].size(); i++)
+    {
+        y = goalPositions[currentStage][i].first;
+        x = goalPositions[currentStage][i].second;
+        if (maps[currentStage][y][x] != 2)
+            return false;
+    }
+    return true;
+}
+
+//해당 좌표 위치가 목적지인지 체크하는 함수
+bool Game::checkGoal(int y, int x)
+{
+    for (int i = 0; i < goalPositions[currentStage].size(); i++)
+        if (goalPositions[currentStage][i].first == y && goalPositions[currentStage][i].second == x)
+            return true;
+    return false;
+}
+
+//빈칸으로 한 칸 앞으로 가는 함수
+void Game::walk(int y, int x, int nextY, int nextX)
+{
+    maps[currentStage][nextY][nextX] = 5;
+    maps[currentStage][y][x] = 0;
+    playerPositions[currentStage].first = nextY;
+    playerPositions[currentStage].second = nextX;
+    stepCount++;
+}
+
+//앞에 상자가 있을 경우 밀면서 한 칸 앞으로 가는 함수
+void Game::push(int y, int x, int nextY, int nextX, int afterY, int afterX)
+{
+    maps[currentStage][afterY][afterX] = 2;
+    maps[currentStage][nextY][nextX] = 5;
+    maps[currentStage][y][x] = 0;
+    playerPositions[currentStage].first = nextY;
+    playerPositions[currentStage].second = nextX;
+    stepCount++;
+    pushCount++;
+}
+
+//게임 시작
+void Game::gameStart()
+{
+    setMaps();
+    setPlayer();
+    this->finished.assign(numOfStages, false);
+    this->currentStage = 0;
+    this->stepCount = 0;
+    this->pushCount = 0;
+}
+
+//맵 위에 플레이어를 위치시킨다.
+void Game::setPlayer()
+{
+    int y, x;
+    ifstream is("PlayerPositions.txt");
+    is >> numOfStages;
+    for (int i = 0; i < numOfStages; i++)
+    {
+        is >> y >> x;
+        playerPositions.push_back(make_pair(y, x));
+    }
+
+    for (int i = 0; i < numOfStages; i++)
+    {
+        for (int j = 0; j < maps[i].size(); j++)
+        {
+            for (int k = 0; k < maps[i][j].size(); k++)
+            {
+                if (j == playerPositions[i].first && k == playerPositions[i].second)
+                    maps[i][j][k] = 5;
+            }
+        }
+    }
+}
+//3차원 벡터에 maps.txt를 가져와 데이터를 저장하는 함수
+void Game::setMaps()
+{
+    int numOfRows, numOfCols;
+    ifstream is("maps.txt");
+    is >> numOfStages;
+    this->maps.assign(numOfStages, vector<vector<int>>());
+    this->goalPositions.assign(numOfStages, vector<pair<int, int>>());
+    for (int i = 0; i < numOfStages; i++)
+    {
+        is >> numOfRows >> numOfCols;
+        this->maps[i].assign(numOfRows, vector<int>(numOfCols, -1));
+        for (int j = 0; j < numOfRows; j++)
+        {
+            for (int k = 0; k < numOfCols; k++)
+            {
+                is >> this->maps[i][j][k];
+                if (maps[i][j][k] == 3)
+                    goalPositions[i].push_back(make_pair(j, k));
+            }
+        }
+    }
+}
+
+int main()
+{
+    Game game;
+    game.gameStart();
+
+    initscr();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    noecho();
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_YELLOW);
+    init_pair(2, COLOR_RED, COLOR_GREEN);
+    bkgd(COLOR_PAIR(1));
+do
+    {
+    attron(COLOR_PAIR(2));
+    border('*', '*', '*', '*', '*', '*', '*', '*');
+    attroff(COLOR_PAIR(2));
+
+    attron(COLOR_PAIR(1));
+
+    
+        char buff[4];
+        sprintf(buff, "%d", game.getStepCount());
+        mvprintw(2, 5, buff);
+
+        sprintf(buff, "%d", game.getPushCount());
+        mvprintw(3, 5, buff);
+
+        for (int j = 0; j < game.getMaps()[game.getCurrentStage()].size(); j++)
+        {
+            for (int k = 0; k < game.getMaps()[game.getCurrentStage()][j].size(); k++)
+            {
+                char buff[1];
+                sprintf(buff, "%d", game.getMaps()[game.getCurrentStage()][j][k]);
+                mvprintw(j + 5, k + 5, buff);
+            }
+        }
+        refresh();
+
+        int input = getch();
+        game.move(input);
+
+        clear();
+        refresh();
+        if (game.getFinised()[game.getCurrentStage()]){
+            
+        }
+    } while (!game.checkAllSuccess());
+
+    attroff(COLOR_PAIR(1));
+
+    endwin();
+}
