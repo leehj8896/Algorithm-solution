ff2ca609cc58d531d16327f8ea526e4db8d0c5d2
leehj8896 <leehj8896@gmail.com>
2020-11-02 01:12:12 +0900

    백준 최소 스패닝 트리

---
+++ 최소 스패닝 트리/solution.py
@@ -0,0 +1,34 @@
+import sys
+
+def get_index(list_cut, node):
+
+    for i in range(len(list_cut)):
+        if node in list_cut[i]:
+            return i
+
+    return -1
+
+v, e = map(int, sys.stdin.readline().rstrip().split(' '))
+
+list_cut = [{i} for i in range(1, v+1)]
+
+list_edge = []
+for i in range(e):
+
+    a, b, c = map(int, sys.stdin.readline().rstrip().split(' '))
+    list_edge.append([a,b,c])
+
+list_edge.sort(key=lambda x: x[2])
+
+result = 0
+for edge in list_edge:
+
+    idx1 = get_index(list_cut, edge[0])
+    idx2 = get_index(list_cut, edge[1])
+
+    if idx1 != idx2:
+        result += edge[2]
+        list_cut[idx1] = list_cut[idx1] | list_cut[idx2]
+        list_cut.pop(idx2)
+
+print(result)
